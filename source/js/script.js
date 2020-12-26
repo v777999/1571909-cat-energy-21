a6d934ca61c8b0b34a624da1c7ba2d922a0c97c6
v777999 <55867385+v777999@users.noreply.github.com>
2020-12-26 21:02:46 +0300

    Status quo on 26-12-20 21:02

---
+++ source/js/script.js
@@ -0,0 +1,28 @@
+var navMain = document.querySelector('.main-nav');
+    var navToggle = document.querySelector('.main-nav__toggle');
+
+    navMain.classList.remove('main-nav--nojs');
+
+    navToggle.addEventListener('click', function () {
+      if (navMain.classList.contains('main-nav--closed')) {
+        navMain.classList.remove('main-nav--closed');
+        navMain.classList.add('main-nav--opened');
+      } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+      }
+    });
+
+    ymaps.ready(init);
+    function init() {
+      var myMap = new ymaps.Map(document.querySelector(".invitation__map"), {
+        center: [59.938635, 30.323118],
+        zoom: 14
+      });
+      myPlacemark = new ymaps.Placemark([59.938635, 30.323118], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/map-pin.png',
+        iconImageSize: [57, 53],
+      });
+      myMap.geoObjects.add(myPlacemark);
+    };
